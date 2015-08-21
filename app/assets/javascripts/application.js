951f7a43dea431093c7ed16362fa2b6793a2e732
maggie-bingham <mbingham4309@gmail.com>
2015-08-20 21:02:23 -0400

    added resource Product added bootstrap for styling Setup navbar so customer can sign in and logout setup index page - navbar, intial grid for products added faker gem and db/seed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require bootstrap
+//= require refile
 //= require_tree .
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+4.times do
+  customer = Customer.create(email: Faker::Internet.email,
+              password: "password",
+              password_confirmation: "password")
+end
+
+Product.create(photo:"http://dealsinseconds.com/wp-content/uploads/2014/06/111.jpg", name: "Converse Shoes", description: "New pair of girl's converse shoes. Size 7.", price: 80)
+Product.create(photo:"http://www.d4products.com/wp-content/uploads/2013/11/sample_item3.png", name: "Nikon Camera", description: "Nikon Digital Camera.  100 Shutter Speed", price: 100)
+Product.create(photo:"http://www.georges.com.au/media/catalog/product/p/x/pxc_450_productimage.jpg", name: "Headphones", description: "Bluetooth headphones", price: 200)
+Product.create(photo:"http://assets.red.org/product_images/873/iphone6plus-pb-svr_iphone6pluscase_silcn_red-print560.png", name: "Iphone 6 Case", description: "New pink iphone 6 case", price: 20)
+Product.create(photo:"http://www.highsnobiety.com/uploads/pics/colette-x-gap-product-2.jpg", name: "Winter Coat", description: "Silver winter coat. Never be cold again.", price: 150)
